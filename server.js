56fd0d5e7dc23219a10afa7e38ac875cb1c3ced2
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-13 15:22:12 -0700

    Implement middleware

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
+
 const express = require('express');
 
+const middleware = require('./middleware/config');
+
 const server = express();
+
+middleware(server);
 
 server.listen(process.env.PORT, () => {
 	console.log('Server now up and running on http://localhost:5000');
