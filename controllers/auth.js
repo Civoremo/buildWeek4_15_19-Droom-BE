3ab8fb44e387171ee72d69930ebc7184eb9109d6
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-13 16:59:05 -0700

    Fix bugs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,21 +1,25 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+
 const Users = require('../models/Users');
+const { generateToken } = require('../helpers/generateToken');
 
 router.post('/register', async (req, res) => {
 	try {
 		let user = req.body;
-		user.password = await bcrypt.hashSync(user.password, 10);
 
-		const newUser = Users.add(user);
-		res.status(201).json(newUser);
+		user.password = await bcrypt.hashSync(user.password, 10);
+		let newUser = await Users.add(user);
+		user = await generateToken(newUser);
+
+		res.status(201).json(user);
 	} catch (err) {
 		res.status(500).json({
 			message:
 				'Sorry, but something went wrong while registering'
 		});
 
-		throw new Error('/register', err);
+		throw new Error(err);
 	}
 });
 
--- database/migrations/20190413151109_users_table.js
+++ database/migrations/20190413151109_users_table.js
@@ -1,5 +1,5 @@
 exports.up = function(knex) {
-	knex.schema.createTable('users', tbl => {
+	return knex.schema.createTable('users', tbl => {
 		tbl.increments();
 
 		tbl.string('email', 128)
@@ -11,5 +11,5 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-	knex.schema.dropTable('users');
+	return knex.schema.dropTable('users');
 };
