0e0cf2b8c40a768a8eafe10d3dfa33352f45f380
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-13 15:37:00 -0700

    Create auth controller

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+
 const middleware = require('./middleware/config');
+const authController = require('./controllers/auth');
 
 const server = express();
 
+// middleware
 middleware(server);
+
+// controllers
+server.use('/api/auth', authController);
 
 server.listen(process.env.PORT, () => {
 	console.log('Server now up and running on http://localhost:5000');
