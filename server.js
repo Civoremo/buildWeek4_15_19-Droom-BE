d1cd63e6516e5729b2516334a28df1a100d99a44
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-13 16:21:37 -0700

    Implement error reporting middleware

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 
 const middleware = require('./middleware/config');
+const errorMiddleware = require('./middleware/errorReporting');
+
 const usersController = require('./controllers/users');
 
 const server = express();
@@ -11,6 +13,9 @@ middleware(server);
 
 // controllers
 server.use('/api/auth', usersController);
+
+// error reporting middleware (Must be after all requests)
+errorMiddleware(server);
 
 server.listen(process.env.PORT, () => {
 	console.log('Server now up and running on http://localhost:5000');
