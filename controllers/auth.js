33b1d436a5dbc2889543705f1a166bc54e8d67fd
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-13 16:48:38 -0700

    Handle register endpoint

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,21 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const Users = require('../models/Users');
 
-router.post('/register', async (req, res) => {});
+router.post('/register', async (req, res) => {
+	try {
+		let user = req.body;
+		user.password = await bcrypt.hashSync(user.password, 10);
+
+		const newUser = Users.add(user);
+		res.status(201).json(newUser);
+	} catch (err) {
+		res.status(500).json({
+			message:
+				'Sorry, but something went wrong while registering'
+		});
+
+		throw new Error('/register', err);
+	}
+});
 
 module.exports = router;
