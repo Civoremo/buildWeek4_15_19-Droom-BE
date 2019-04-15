ff67bb029b933086ce508678ec0fca67f624cba4
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-15 11:59:43 -0700

    Create seeker skills migration

---
+++ database/migrations/20190415115520_seeker_skills_table.js
@@ -0,0 +1,19 @@
+exports.up = function(knex) {
+	return knex.schema.createTable('seeker_skills', tbl => {
+		tbl.increments();
+
+		tbl.integer('seekerId', 128)
+			.unsigned()
+			.notNullable()
+			.references('id')
+			.inTable('seekers')
+			.onDelete('CASCADE')
+			.onUpdate('CASCADE');
+
+		tbl.string('seekerSkill').notNullable();
+	});
+};
+
+exports.down = function(knex) {
+	return knex.schema.dropTable('seeker_skills');
+};
