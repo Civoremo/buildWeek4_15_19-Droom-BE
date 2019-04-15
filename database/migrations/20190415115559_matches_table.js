732c71b7016821d72bca604243e458afa3daebfe
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-15 11:59:51 -0700

    Create matches migration

---
+++ database/migrations/20190415115559_matches_table.js
@@ -0,0 +1,25 @@
+exports.up = function(knex) {
+	return knex.schema.createTable('matches', tbl => {
+		tbl.increments();
+
+		tbl.integer('jobId', 128)
+			.unsigned()
+			.notNullable()
+			.references('id')
+			.inTable('jobs')
+			.onDelete('CASCADE')
+			.onUpdate('CASCADE');
+
+		tbl.integer('seekerId', 128)
+			.unsigned()
+			.notNullable()
+			.references('id')
+			.inTable('seekers')
+			.onDelete('CASCADE')
+			.onUpdate('CASCADE');
+	});
+};
+
+exports.down = function(knex) {
+	return knex.schema.dropTable('matches');
+};
