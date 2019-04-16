2be5efc2dbb856cbc9e6c918502a64000691a311
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-16 13:11:09 -0700

    Define add seeker experience endpoint

--- controllers/experience.js
+++ controllers/experience.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
 const Experience = require('../models/Experience');
 
+// Add seeker experience
+router.post('/', async (req, res) => {});
+
 module.exports = router;
