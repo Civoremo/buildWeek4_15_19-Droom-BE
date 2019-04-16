713d2de657e219d51e96f813f9e36426b367bbc5
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-16 10:30:13 -0700

    Create add education method

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const errorMiddleware = require('./middleware/errorReporting');
 
 const authController = require('./controllers/auth');
 const seekerController = require('./controllers/seekers');
+const educationController = require('./controllers/education');
 const comController = require('./controllers/companies');
 
 // initializations
@@ -21,6 +22,7 @@ middleware(server);
 // controllers
 server.use('/api/auth', authController);
 server.use('/api/seekers', seekerController);
+server.use('/api/education', educationController);
 server.use('/api/companies', comController);
 
 // error reporting middleware (Must be after all requests)
