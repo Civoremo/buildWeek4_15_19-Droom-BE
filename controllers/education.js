b461007efb0cea4b97fd133b1fa540dc60137752
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-16 12:15:02 -0700

    Create education endpoint

--- controllers/education.js
+++ controllers/education.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const Education = require('../models/Education');
 
+// Create seeker education
 router.post('/', async (req, res) => {
 	try {
 		const education = await Education.add(req.body);
@@ -10,6 +11,21 @@ router.post('/', async (req, res) => {
 		res.status(500).json({
 			message:
 				'Sorry, but something went wrong while trying to add education'
+		});
+
+		throw new Error(err);
+	}
+});
+
+// Get all job seeker education
+router.get('/:id', async (req, res) => {
+	try {
+		const education = await Education.find(req.params.id);
+		res.status(200).json(education);
+	} catch (err) {
+		res.status(500).json({
+			message:
+				'Sorry, but something went wrong while trying to get education'
 		});
 
 		throw new Error(err);
