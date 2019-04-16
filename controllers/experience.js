9a0d6c00a66035f5bbd39207893662f02d2e73e7
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-16 13:24:17 -0700

    Handle add seeker experience endpoint

--- controllers/experience.js
+++ controllers/experience.js
@@ -2,7 +2,19 @@ const router = require('express').Router();
 const Experience = require('../models/Experience');
 
 // Add seeker experience
-router.post('/', async (req, res) => {});
+router.post('/', async (req, res) => {
+	try {
+		const experience = await Experience.add(req.body);
+		res.status(201).json(experience);
+	} catch (err) {
+		res.status(500).json({
+			message:
+				'Sorry, but something went wrong while trying to add experience'
+		});
+
+		throw new Error(err);
+	}
+});
 
 // Get all seeker experiences
 router.get('/', async (req, res) => {});
