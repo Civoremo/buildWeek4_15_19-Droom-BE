4894ecbd282147fd5d154c40c6efbcb3630d8e99
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-16 13:13:00 -0700

    Define update individual experience endpoint:

--- controllers/experience.js
+++ controllers/experience.js
@@ -4,7 +4,10 @@ const Experience = require('../models/Experience');
 // Add seeker experience
 router.post('/', async (req, res) => {});
 
-// Get all seeker experience
+// Get all seeker experiences
 router.get('/', async (req, res) => {});
+
+// Update individual experience
+router.put('/', async (req, res) => {});
 
 module.exports = router;
