a28940f20e41a4094ad91d11c5befea8fde27517
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-16 13:23:55 -0700

    Create add experience method

---
+++ models/Experience.js
@@ -0,0 +1,32 @@
+const db = require('../database/dbConfig');
+
+module.exports = {
+	add
+};
+
+// Add experience to seeker profile
+async function add({ userId, seekerExperience }) {
+	let { id } = await findSeeker(userId);
+	seekerId = id;
+
+	// add seekerId prop to experience object
+	const updatedExperience = seekerExperience.map(edu => {
+		return { seekerId, ...edu };
+	});
+
+	// add experience to db
+	await db('experience')
+		.insert(updatedExperience)
+		.returning('id');
+
+	// return all experience related to seeker profile
+	return db('experience').where({ seekerId });
+}
+
+// Find seekerId by userId
+function findSeeker(id) {
+	return db('seekers')
+		.where({ userId: id })
+		.select('id')
+		.first();
+}
