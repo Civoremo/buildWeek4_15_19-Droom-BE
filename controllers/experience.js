c23cb644d7ef636833648da8b350dbf89be750dd
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-16 13:12:07 -0700

    Define get all seeker experiences endpoint

--- controllers/experience.js
+++ controllers/experience.js
@@ -4,4 +4,7 @@ const Experience = require('../models/Experience');
 // Add seeker experience
 router.post('/', async (req, res) => {});
 
+// Get all seeker experience
+router.get('/', async (req, res) => {});
+
 module.exports = router;
