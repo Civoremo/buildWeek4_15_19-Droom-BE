5a24d9f61fb4f7c5845f0e53f75985402f02f799
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-15 17:54:34 -0700

    Handle seeker endpoint

--- controllers/seekers.js
+++ controllers/seekers.js
@@ -1,5 +1,18 @@
 const router = require('express').Router();
+const Seekers = require('../models/Seekers');
 
-router.post('/', async (req, res) => {});
+router.post('/', async (req, res) => {
+	try {
+		const profile = Seekers.add(req.body);
+		res.status(201).json(profile);
+	} catch (err) {
+		res.status(500).json({
+			message:
+				'Sorry, but something went wrong while trying to create that profile'
+		});
+
+		throw new Error(err);
+	}
+});
 
 module.exports = router;
