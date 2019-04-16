593459f79d5dcd0a4d398f8fd35f7a0a2f4407b4
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-16 14:13:55 -0700

    Handle get skills endpoint

--- controllers/skills.js
+++ controllers/skills.js
@@ -11,15 +11,24 @@ router.post('/', async (req, res) => {
 			message:
 				'Sorry, but something went wrong while trying to add skills'
 		});
-
-		console.log(err);
-
 		throw new Error(err);
 	}
 });
 
 // Get skills by id
-router.get('/:id', async (req, res) => {});
+router.get('/:id', async (req, res) => {
+	try {
+		const skills = await Skills.find(req.params.id);
+		res.status(200).json(skills);
+	} catch (err) {
+		res.status(500).json({
+			message:
+				'Sorry, but something went wrong while trying to get skills'
+		});
+
+		throw new Error(err);
+	}
+});
 
 // Update skill
 router.put('/:id', async (req, res) => {});
