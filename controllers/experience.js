57c2eca26aac3f51bb12755d60f0bdeccb472950
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-16 13:30:17 -0700

    Handle update experience endpoint

--- controllers/experience.js
+++ controllers/experience.js
@@ -32,9 +32,24 @@ router.get('/:id', async (req, res) => {
 });
 
 // Update individual experience
-router.put('/', async (req, res) => {});
+router.put('/:id', async (req, res) => {
+	try {
+		const updatedExperience = await Experience.update(
+			req.params.id,
+			req.body
+		);
+		res.status(200).json(updatedExperience);
+	} catch (err) {
+		res.status(500).json({
+			message:
+				'Sorry, but something went wrong while updating experience'
+		});
+
+		throw new Error(err);
+	}
+});
 
 // Delete individual experience
-router.delete('/', async (req, res) => {});
+router.delete('/:id', async (req, res) => {});
 
 module.exports = router;
