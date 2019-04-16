a6fe1454ff0f7923b56b8bfdb7a65e71ecaf8053
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-16 13:49:23 -0700

    Define get skills endpoint

--- controllers/skills.js
+++ controllers/skills.js
@@ -4,4 +4,7 @@ const Skills = require('../models/Skills');
 // Create skills
 router.post('/', async (req, res) => {});
 
+// Get skills by id
+router.get('/:id', async (req, res) => {});
+
 module.exports = router;
