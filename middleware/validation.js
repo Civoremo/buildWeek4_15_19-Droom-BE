4ad05b8d3960a0a160d373436b3023105287cef4
AAsriyan <aasriyan@protonmail.com>
2019-04-17 11:53:26 -0400

    changed validation issues

--- middleware/validation.js
+++ middleware/validation.js
@@ -17,7 +17,7 @@ module.exports = {
 };
 
 function companyValidation(req, res, next) {
-	const { userId, company } = req.body;
+	const { userId, companies } = req.body;
 
 	const {
 		companyName,
@@ -26,7 +26,7 @@ function companyValidation(req, res, next) {
 		state,
 		city,
 		zipcode
-	} = company;
+	} = companies;
 
 	if (!userId) {
 		return res.status(400).json({
