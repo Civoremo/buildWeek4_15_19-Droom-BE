9fea317d6555e43713343fceada6f943a59bb040
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-16 23:42:46 -0700

    Create skills validation

--- middleware/validation.js
+++ middleware/validation.js
@@ -8,7 +8,9 @@ module.exports = {
 	educationValidation,
 	updateEducationValidation,
 	experienceValidation,
-	updateExperienceValidation
+	updateExperienceValidation,
+	skillsValidation,
+	updateSkillsValidation
 };
 
 function companyValidation(req, res, next) {
@@ -571,6 +573,34 @@ function updateExperienceValidation(req, res, next) {
 			message: 'Please provide a end date'
 		});
 	}
+
+	next();
+}
+
+function skillsValidation(req, res, next) {
+	const { userId, seekerSkills } = req.body.skills;
+
+	if (!userId) {
+		return res.status(400).json({
+			message: 'Please provide a user id'
+		});
+	}
+
+	if (!seekerSkills.length)
+		return res.status(400).json({
+			message: 'Please provide some skills'
+		});
+
+	next();
+}
+
+function updateSkillsValidation(req, res, next) {
+	const { seekerSkill } = req.body;
+
+	if (!seekerSkill)
+		return res.status(400).json({
+			message: 'Please provide a skill'
+		});
 
 	next();
 }
