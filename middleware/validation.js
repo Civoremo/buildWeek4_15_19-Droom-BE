b67635ce7ed6cba76c1b9a6546b3a97426e2f604
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-16 23:02:02 -0700

    Create experience validation

--- middleware/validation.js
+++ middleware/validation.js
@@ -5,9 +5,9 @@ module.exports = {
 	authValidation,
 	seekerValidation,
 	updateSeekerValidation,
-
 	educationValidation,
-	updateEducationValidation
+	updateEducationValidation,
+	experienceValidation
 };
 
 function companyValidation(req, res, next) {
@@ -472,6 +472,100 @@ function updateEducationValidation(req, res, next) {
 	}
 
 	if (!eduEnd) {
+		return res.status(400).json({
+			message: 'Please provide a end date'
+		});
+	}
+
+	next();
+}
+
+function experienceValidation(req, res, next) {
+	const { userId, seekerExperience } = req.body;
+
+	seekerExperience.map(exp => {
+		const {
+			jobTitle,
+			jobCompany,
+			jobDescription,
+			jobStart,
+			jobEnd
+		} = exp;
+
+		if (!userId) {
+			return res.status(400).json({
+				message: 'Please provide a user id'
+			});
+		}
+
+		if (!jobTitle) {
+			return res.status(400).json({
+				message: 'Please provide a job title'
+			});
+		}
+
+		if (!jobCompany) {
+			return res.status(400).json({
+				message: 'Please provide a company'
+			});
+		}
+
+		if (!jobDescription) {
+			return res.status(400).json({
+				message: 'Please provide a job description'
+			});
+		}
+
+		if (!jobStart) {
+			return res.status(400).json({
+				message: 'Please provide a start date'
+			});
+		}
+
+		if (!jobEnd) {
+			return res.status(400).json({
+				message: 'Please provide a end date'
+			});
+		}
+	});
+
+	next();
+}
+
+function updateEducationValidation(req, res, next) {
+	const {
+		jobTitle,
+		jobCompany,
+		jobDescription,
+		jobStart,
+		jobEnd
+	} = req.body;
+
+	if (!jobTitle) {
+		return res.status(400).json({
+			message: 'Please provide a job title'
+		});
+	}
+
+	if (!jobCompany) {
+		return res.status(400).json({
+			message: 'Please provide a company'
+		});
+	}
+
+	if (!jobDescription) {
+		return res.status(400).json({
+			message: 'Please provide a job description'
+		});
+	}
+
+	if (!jobStart) {
+		return res.status(400).json({
+			message: 'Please provide a start date'
+		});
+	}
+
+	if (!jobEnd) {
 		return res.status(400).json({
 			message: 'Please provide a end date'
 		});
