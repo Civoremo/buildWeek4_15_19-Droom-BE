18d8fe14ab1c92faf758332551f4ba79221483f5
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-17 16:09:25 -0700

    Create matches endpoint

--- controllers/matches.js
+++ controllers/matches.js
@@ -1,10 +1,20 @@
 const router = require('express').Router();
 const Matches = require('../models/Matches');
+const Seekers = require('../models/Seekers');
 
 router.get('/:id', async (req, res) => {
 	try {
+		const profile = await Seekers.findById(req.params.id);
+
+		if (!profile)
+			return res
+				.status(404)
+				.json({
+					message:
+						"Sorry, but that user doesn't have a profile"
+				});
+
 		const matches = await Matches.get(req.params.id);
-		console.log(matches);
 		res.status(200).json(matches);
 	} catch (err) {
 		res.status(500).json({
