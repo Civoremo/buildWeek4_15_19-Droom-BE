8f760200faef417a8430c2fb81cf1fdc02a56d1e
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-16 22:41:09 -0700

    Create education and seeker validation

--- middleware/validation.js
+++ middleware/validation.js
@@ -4,7 +4,9 @@ module.exports = {
 	jobValidation,
 	authValidation,
 	seekerValidation,
-	updateSeekerValidation
+	updateSeekerValidation,
+	educationValidation,
+	updateEducationValidation
 };
 
 function companyValidation(req, res, next) {
@@ -377,6 +379,100 @@ function updateSeekerValidation(req, res, next) {
 	if (!zipcode) {
 		return res.status(400).json({
 			message: 'Please provide a zip code'
+		});
+	}
+
+	next();
+}
+
+function educationValidation(req, res, next) {
+	const { userId, seekerEducation } = req.body;
+
+	seekerEducation.map(edu => {
+		const {
+			eduSchool,
+			eduCredential,
+			eduDescription,
+			eduStart,
+			eduEnd
+		} = edu;
+
+		if (!userId) {
+			return res.status(400).json({
+				message: 'Please provide a user id'
+			});
+		}
+
+		if (!eduSchool) {
+			return res.status(400).json({
+				message: 'Please provide a school'
+			});
+		}
+
+		if (!eduCredential) {
+			return res.status(400).json({
+				message: 'Please provide a credential'
+			});
+		}
+
+		if (!eduDescription) {
+			return res.status(400).json({
+				message: 'Please provide a description'
+			});
+		}
+
+		if (!eduStart) {
+			return res.status(400).json({
+				message: 'Please provide a start date'
+			});
+		}
+
+		if (!eduEnd) {
+			return res.status(400).json({
+				message: 'Please provide a end date'
+			});
+		}
+	});
+
+	next();
+}
+
+function updateEducationValidation(req, res, next) {
+	const {
+		eduSchool,
+		eduCredential,
+		eduDescription,
+		eduStart,
+		eduEnd
+	} = req.body;
+
+	if (!eduSchool) {
+		return res.status(400).json({
+			message: 'Please provide a school'
+		});
+	}
+
+	if (!eduCredential) {
+		return res.status(400).json({
+			message: 'Please provide a credential'
+		});
+	}
+
+	if (!eduDescription) {
+		return res.status(400).json({
+			message: 'Please provide a description'
+		});
+	}
+
+	if (!eduStart) {
+		return res.status(400).json({
+			message: 'Please provide a start date'
+		});
+	}
+
+	if (!eduEnd) {
+		return res.status(400).json({
+			message: 'Please provide a end date'
 		});
 	}
 
