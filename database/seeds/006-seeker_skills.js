c2ac63b4e41776024c61eec441dc37acd322a79c
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-17 13:20:01 -0700

    Create seeker skills seed

--- database/seeds/006-seeker_skills.js
+++ database/seeds/006-seeker_skills.js
@@ -1,51 +1,131 @@
 exports.seed = function(knex, Promise) {
-	return knex('education').insert([
+	return knex('seeker_skills').insert([
 		{
-			skills: {
-				userId: 1,
-				seekerSkills: [
-					'Java',
-					'JavaScript',
-					'jQuery',
-					'HTML',
-					'CSS',
-					'Bootstrap',
-					'Git'
-				]
-			}
+			seekerId: 1,
+			seekerSkill: 'Java'
 		},
 		{
-			skills: {
-				userId: 2,
-				seekerSkills: [
-					'Golang',
-					'Python',
-					'JavaScript',
-					'Node',
-					'Express',
-					'C++',
-					'C#',
-					'MySQL',
-					'PostgreSQL',
-					'Git',
-					'HTML',
-					'React',
-					'CSS',
-					'Django'
-				]
-			}
+			seekerId: 1,
+			seekerSkill: 'JavaScript'
 		},
 		{
-			skills: {
-				userId: 3,
-				seekerSkills: [
-					'Java',
-					'Spring Framework',
-					'React',
-					'MySQL',
-					'PostgreSQL'
-				]
-			}
+			seekerId: 1,
+			seekerSkill: 'Kafka'
+		},
+		{
+			seekerId: 1,
+			seekerSkill: 'jQuery'
+		},
+		{
+			seekerId: 1,
+			seekerSkill: 'HTML'
+		},
+
+		{
+			seekerId: 1,
+			seekerSkill: 'CSS'
+		},
+
+		{
+			seekerId: 1,
+			seekerSkill: 'Bootstrap'
+		},
+
+		{
+			seekerId: 1,
+			seekerSkill: 'Git'
+		},
+		{
+			seekerId: 2,
+			seekerSkill: 'Golang'
+		},
+		{
+			seekerId: 2,
+			seekerSkill: 'Python'
+		},
+		{
+			seekerId: 2,
+			seekerSkill: 'JavaScript'
+		},
+		{
+			seekerId: 2,
+			seekerSkill: 'Node'
+		},
+		{
+			seekerId: 2,
+			seekerSkill: 'Express'
+		},
+		{
+			seekerId: 2,
+			seekerSkill: 'C++'
+		},
+		{
+			seekerId: 2,
+			seekerSkill: 'C#'
+		},
+		{
+			seekerId: 2,
+			seekerSkill: 'MySQL'
+		},
+		{
+			seekerId: 2,
+			seekerSkill: 'PostgreSQL'
+		},
+		{
+			seekerId: 2,
+			seekerSkill: 'HTML'
+		},
+		{
+			seekerId: 2,
+			seekerSkill: 'Git'
+		},
+		{
+			seekerId: 2,
+			seekerSkill: 'React'
+		},
+		{
+			seekerId: 2,
+			seekerSkill: 'CSS'
+		},
+		{
+			seekerId: 2,
+			seekerSkill: 'Django'
+		},
+		{
+			seekerId: 2,
+			seekerSkill: 'Redux'
+		},
+		{
+			seekerId: 2,
+			seekerSkill: 'Webpack'
+		},
+		{
+			seekerId: 2,
+			seekerSkill: 'Git'
+		},
+		{
+			seekerId: 3,
+			seekerSkill: 'Java'
+		},
+		{
+			seekerId: 3,
+			seekerSkill: 'Spring Framework'
+		},
+		{
+			seekerId: 3,
+			seekerSkill: 'React'
+		},
+		{
+			seekerId: 3,
+			seekerSkill: 'MySQL'
+		},
+		{
+			seekerId: 3,
+			seekerSkill: 'PostgreSQL'
+		},
+		{
+			seekerId: 3,
+			seekerSkill: 'Ruby'
 		}
 	]);
 };
