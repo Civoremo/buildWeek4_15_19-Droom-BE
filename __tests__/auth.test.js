5f26c9cabe3fee6a4b72a633d715bb540fb5961e
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-17 21:56:01 -0700

    Create login tests

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -46,4 +46,50 @@ describe('Authentication endpoint tests', () => {
 			expect(response.body.token).toBe(response.body.token);
 		});
 	});
+
+	describe('POST /login', () => {
+		it('should return status code of 400 (Bad Request) if there are any missing fields', async () => {
+			const user = {
+				password: 'password'
+			};
+
+			const response = await request(server)
+				.post('/api/auth/login')
+				.send(user);
+
+			expect(response.status).toBe(400);
+		});
+
+		it('should return status code of 200 (OK) after logging in a user', async () => {
+			const user = {
+				email: 'john@gmail.com',
+				password: 'password'
+			};
+
+			let response = await request(server)
+				.post('/api/auth/register')
+				.send(user);
+
+			expect(response.status).toBe(201);
+
+			response = await request(server)
+				.post('/api/auth/login')
+				.send(user);
+
+			expect(response.status).toBe(200);
+		});
+
+		it('should return a token after a user is successfully logged in', async () => {
+			const user = {
+				email: 'john@gmail.com',
+				password: 'password'
+			};
+
+			const response = await request(server)
+				.post('/api/auth/login')
+				.send(user);
+
+			expect(response.body.token).toBe(response.body.token);
+		});
+	});
 });
