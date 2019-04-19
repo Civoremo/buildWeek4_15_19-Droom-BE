d3c2fb533e10ddffd92b46861dd18a7c584920d0
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-19 08:28:23 -0700

    Fix user model

--- database/seeds/001-users.js
+++ database/seeds/001-users.js
@@ -4,55 +4,81 @@ exports.seed = function(knex, Promise) {
 	return knex('users').insert([
 		{
 			email: 'microsoft@microsoft.com',
-			password: bcrypt.hashSync('password', 10)
+			password: bcrypt.hashSync('password', 10),
+			seeker: false,
+			employer: true
 		},
 		{
 			email: 'apple@apple.com',
-			password: bcrypt.hashSync('password', 10)
+			password: bcrypt.hashSync('password', 10),
+			seeker: false,
+			employer: true
 		},
 		{
 			email: 'github@github.com',
-			password: bcrypt.hashSync('password', 10)
+			password: bcrypt.hashSync('password', 10),
+			seeker: false,
+			employer: true
 		},
 		{
 			email: 'nexient@nexient.com',
-			password: bcrypt.hashSync('password', 10)
+			password: bcrypt.hashSync('password', 10),
+			seeker: false,
+			employer: true
 		},
 		{
 			email: 'netflix@netflix.com',
-			password: bcrypt.hashSync('password', 10)
+			password: bcrypt.hashSync('password', 10),
+			seeker: false,
+			employer: true
 		},
 		{
 			email: 'twitter@twitter.com',
-			password: bcrypt.hashSync('password', 10)
+			password: bcrypt.hashSync('password', 10),
+			seeker: false,
+			employer: true
 		},
 		{
 			email: 'spotify@spotify.com',
-			password: bcrypt.hashSync('password', 10)
+			password: bcrypt.hashSync('password', 10),
+			seeker: false,
+			employer: true
 		},
 		{
 			email: 'facebook@facebook.com',
-			password: bcrypt.hashSync('password', 10)
+			password: bcrypt.hashSync('password', 10),
+			seeker: false,
+			employer: true
 		},
 		{
 			email: 'reddit@reddit.com',
-			password: bcrypt.hashSync('password', 10)
+			password: bcrypt.hashSync('password', 10),
+			seeker: false,
+			employer: true
 		},
 		{
 			email: 'google@google.com',
-			password: bcrypt.hashSync('password', 10)
+			password: bcrypt.hashSync('password', 10),
+			seeker: false,
+			employer: true
 		},
 		{
 			email: 'sam@gmail.com',
-			password: bcrypt.hashSync('password', 10)
+			password: bcrypt.hashSync('password', 10),
+			seeker: true,
+			employer: false
 		},
 		{
 			email: 'john@gmail.com',
-			password: bcrypt.hashSync('password', 10)
+			password: bcrypt.hashSync('password', 10),
+			seeker: true,
+			employer: false
 		},
 		{
 			email: 'sally@gmail.com',
-			password: bcrypt.hashSync('password', 10)
+			password: bcrypt.hashSync('password', 10),
+			seeker: true,
+			employer: false
 		}
 	]);
 };
